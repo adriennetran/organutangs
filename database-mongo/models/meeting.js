dab6b6651d02ce1e71c25a5ba9e4fa209ebee72a
Adrienne Tran <adrienne@adriennetran.com>
2017-08-04 16:02:12 -0400

    non unique user ids

--- database-mongo/models/meeting.js
+++ database-mongo/models/meeting.js
@@ -7,7 +7,7 @@ var axios = require('axios');
 //this schema is used to set up a meeting
 var MeetingSchema = schema({
   meetingId: schema.Types.ObjectId,
-  userId: { type: String, unique: true },
+  userId: { type: String },
   userLocation: { address: String, coordinates: [ Number, Number ] },
   friendId: String,
   updated: { type: Date, default: Date.now }
