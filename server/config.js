12e494896880a8e2feeb503aebbda266b036bc87
Adrienne Tran <adrienne@adriennetran.com>
2017-08-07 10:09:35 -0400

    Latest commit

--- server/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "google" : {
-    "APIKEY": 'AIzaSyAPJ0ticaekx9-Q9Pa34WNCTjGhCSkN9Wo'
-  },
-  "yelpClientID": "TuI7KPQ2iOcLaRBdE3LpMA",
-  "yelpClientSecret": "25iWAWa6iRNP4dTp8mqJoJFqx7Uk1RLWonTmED5w1qrUR8OBkPgC1W5bbvj5otP5"
-};
